351ad0e663ebab80f85541a2c7c9418930f5a71d
janelllantana <janell.lantana@gmail.com>
2016-07-30 17:49:26 -0400

    Initial Commit

---
+++ My Third Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  My Third Project
+//
+//  Created by Janell Lantana on 7/30/16.
+//  Copyright © 2016 janelllantana. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
