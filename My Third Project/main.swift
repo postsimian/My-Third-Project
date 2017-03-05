1e2810d77f61ec6a308b4ee56e4373d8f7754b86
postsimian <postsimian@gmail.com>
2017-03-04 20:58:46 -0500

    First Commit
    Testing a commit

--- My Third Project/main.swift
+++ My Third Project/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+let answer = randomIntBetween(0, high:100)
+var turn = 1
 
+while(true) {
+print("Guess #\(turn): Enter a number between 1 and 100.")
+
+
+let userInput = input()
+let inputAsInt = Int(userInput)
+
+if let guess = inputAsInt {
+    
+    if(guess > answer) {
+        print("Lower!")
+    }
+    else if(guess < answer) {
+        print("Higher!")
+    }
+    else {
+        print("Correct! The answer was \(answer).")
+        break
+    }
+} else {
+    print("Invalid input! Please enter a number.")
+    continue
+}
+turn  = turn + 1
+}
+
+print("It took you \(turn) tries.")
